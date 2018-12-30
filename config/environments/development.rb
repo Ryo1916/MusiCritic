ea98f52288ff1754868ec94aa928f61e7b542b7b
Ryo1916 <telemon1916@gmail.com>
2018-12-30 17:55:35 +0900

    Set letter opener configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,10 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  # ActionMailer and letter_opener
+  config.action_mailer.raise_delivery_errors = true    # development
+  # config.action_mailer.raise_delivery_errors = false # production
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3003 }
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,5 +54,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
