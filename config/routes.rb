3de87b5d2aef3f959c5a7a191dba59ec37bdd53e
Ryo1916 <telemon1916@gmail.com>
2019-01-10 00:20:03 +0900

    Delete users resources

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
                              passwords: "users/passwords",
                              registrations: 'users/registrations',
                              sessions: 'users/sessions' }
-  resources :users, :only => [:show, :index]
   resources :reviews
   resources :artists
   resources :albums
@@ -16,5 +15,5 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
