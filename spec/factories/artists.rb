22bbc11bf0b465bf0199586f3dbb14e674c6d3a0
Ryo1916 <telemon1916@gmail.com>
2019-01-15 22:43:45 +0900

    Set test data for artist model

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :artist do
-    
+    artist { Faker::Music.band }
+    artist_image { Faker::Avator.image }
+  end
+
+  factory :user do
+    name "josie"
+    email "josie@test.abc"
+    password "password"
+    password_confirmation "password"
+    confirmed_at Time.now
   end
 end
