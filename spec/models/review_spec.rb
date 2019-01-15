ba574333159db3d036e24f55de89f3fa9e120812
Ryo1916 <telemon1916@gmail.com>
2019-01-15 22:41:35 +0900

    Set numericality validation

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Review, type: :model do
   # Validations test
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:rating) }
+
+  it do
+    should validate_numericality_of(:rating).
+      is_greater_than_or_equal_to(0).
+      is_less_than_or_equal_to(5)
+  end
+
   it { should validate_presence_of(:text) }
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:album_id) }
