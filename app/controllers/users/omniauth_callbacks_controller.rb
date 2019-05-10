763956451a5d448737e94d86bd6f8155a71b50de
Ryo1916 <telemon1916@gmail.com>
2019-05-10 17:13:19 +0900

    Set facebook method for login and registration

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  before_action :user_signed_in?
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
+    if @user.nil?
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    else
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+    end
+  end
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
+  # GET|POST /users/auth/facebook/callback
+  def failure
+    redirect_to new_user_registration_url
+  end
 
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  protected
 
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+    def after_sign_in_path_for(resource)
+      user_path(resource)
+    end
 end
