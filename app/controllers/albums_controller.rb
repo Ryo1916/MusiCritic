0a4a335422631cb5dccddb219d4c8505cd7a6985
Ryo1916 <telemon1916@gmail.com>
2019-07-05 19:41:34 +0900

    Call and save api data if DB has no albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,7 +3,24 @@ class AlbumsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @albums = Album.all
+    @albums = Album.all.order(name: 'ASC')
+    if params[:album_name]
+      @albums = Album.search_album(album_name: params[:album_name])
+
+      # もしartistがDBに存在しない場合、albumを保存する前にAPIからデータ取得して保存する
+      if @albums.empty?
+        albums = Album.search_album_from_api(album_name: params[:album_name])
+        albums.each do |album|
+          album.artists.each do |artist|
+            if Artist.find_by(name: artist.name).nil?
+              artists = Artist.search_artist_from_api(artist_name: artist.name)
+              Artist.save_artist(artists: artists, artist_name: artist.name)
+            end
+          end
+        end
+        @albums = Album.save_album(albums: albums, album_name: params[:album_name])
+      end
+    end
   end
 
   def show
