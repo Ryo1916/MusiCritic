d2ebb958c1f6cd86b05b141f7a59a04b910f26be
Ryo1916 <telemon1916@gmail.com>
2019-07-05 20:31:41 +0900

    Delete default comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,30 +2,22 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /reviews
-  # GET /reviews.json
   def index
     @reviews = Review.all
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
   def show
   end
 
-  # GET /reviews/new
   def new
     @review = Review.new
     @album = Album.find(params[:id])
   end
 
-  # GET /reviews/1/edit
   def edit
     @album = Album.find(@review.album_id)
   end
 
-  # POST /reviews
-  # POST /reviews.json
   def create
     @review = Review.new(review_params)
     @album = Album.find(@review.album_id)
@@ -41,8 +33,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reviews/1
-  # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
@@ -55,8 +45,6 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
   def destroy
     @review.destroy
     respond_to do |format|
@@ -66,12 +54,11 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:title, :rating, :text, :user_id, :album_id)
     end
