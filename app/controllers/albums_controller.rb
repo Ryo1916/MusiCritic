222cf700cb900394cf679a82c6f5bd84aa5b628f
Ryo1916 <telemon1916@gmail.com>
2019-07-05 19:37:16 +0900

    Delete unuse methods and comments

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,14 +2,10 @@ class AlbumsController < ApplicationController
   before_action :set_album, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /albums
-  # GET /albums.json
   def index
     @albums = Album.all
   end
 
-  # GET /albums/1
-  # GET /albums/1.json
   def show
     @album = Album.find(params[:id])
     @reviews = Review.where(album_id: @album.id).order("created_at desc")
@@ -23,48 +19,6 @@ class AlbumsController < ApplicationController
     end
   end
 
-  # GET /albums/new
-  def new
-    @album = Album.new
-    @album.songs.build
-  end
-
-  # GET /albums/1/edit
-  def edit
-  end
-
-  # POST /albums
-  # POST /albums.json
-  def create
-    @album = Album.new(album_params)
-
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to @album, notice: 'Album was successfully created.' }
-        format.json { render :show, status: :created, location: @album }
-      else
-        format.html { render :new }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /albums/1
-  # PATCH/PUT /albums/1.json
-  def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
-        format.json { render :show, status: :ok, location: @album }
-      else
-        format.html { render :edit }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /albums/1
-  # DELETE /albums/1.json
   def destroy
     @album.destroy
     respond_to do |format|
@@ -74,16 +28,8 @@ class AlbumsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_album
-      @album = Album.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def album_params
-      params.require(:album).permit(
-        :album_name, :album_image, :artist_id, :release, :genre, :description,
-        songs_attributes: [:id, :song_name, :track_num, :artist_id, :_destroy]
-      )
-    end
+  def set_album
+    @album = Album.find(params[:id])
+  end
 end
