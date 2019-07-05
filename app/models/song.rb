f409f0a8289f53d203fa53d6adc3a9973f092d0c
Ryo1916 <telemon1916@gmail.com>
2019-07-05 19:00:42 +0900

    Add annotation

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,12 @@
 #
 # Table name: songs
 #
-#  id         :bigint(8)        not null, primary key
-#  song_name  :string(255)
-#  track_num  :integer
-#  artist_id  :bigint(8)
-#  album_id   :bigint(8)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint(8)        not null, primary key
+#  name         :string(255)
+#  track_number :integer
+#  album_id     :bigint(8)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 
 class Song < ApplicationRecord
