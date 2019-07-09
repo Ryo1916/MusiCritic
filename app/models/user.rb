34ad49b4b262ee9eb44a83422117be94fc23fede
Ryo1916 <telemon1916@gmail.com>
2019-07-09 17:29:06 +0900

    Integrate name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,7 @@ class User < ApplicationRecord
 
   # Validations
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true, uniqueness: true, on: :create
-  validates :name, presence: true, uniqueness: true, on: :update
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
   # Omniauth
