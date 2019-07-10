1b3d561900f7e3a9a816a3622cb83c53becf0b7e
Ryo1916 <telemon1916@gmail.com>
2019-07-10 14:49:52 +0900

    Set user_params to update user profile without change their password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,23 @@ class UsersController < ApplicationController
 
   def edit; end
 
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+      flash[:notice] = 'Your profile was successfully updated.'
+    else
+      render :edit
+      flash[:alert] = 'Update failed.'
+    end
+  end
+
   private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :avatar)
     end
 end
