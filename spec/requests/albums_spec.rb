dcb0b0beed5717f7168bb31071d5bf82868eb461
Ryo1916 <telemon1916@gmail.com>
2019-07-10 20:06:41 +0900

    Change verbs to the third person singular, delete unuse comments

--- spec/requests/albums_spec.rb
+++ spec/requests/albums_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Album', type: :request do
-  # Test suite for GET /albums
   describe 'GET /albums' do
     context 'with authentication' do
       login
-      it 'return success status' do
+      it 'returns success status' do
         get '/albums'
         expect(response).to have_http_status(:success)
       end
 
-      it 'get albums page' do
+      it 'gets albums page' do
         get '/albums'
         expect(response).to render_template(:index)
       end
     end
 
     context 'without authentication' do
-      it 'return fail status' do
+      it 'returns fail status' do
         get '/albums'
         expect(response).to have_http_status(302)
       end
--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -1,27 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Artist', type: :request do
-  # initialize test data
-  # let!(:artists) { create_list(:artist, 10) }
-  # let(:artist_id) { artists.first.id }
-
-  # Test suite for GET /artists
   describe 'GET /artists' do
     context 'with authentication' do
       login
-      it 'return success status' do
+      it 'returns success status' do
         get '/artists'
         expect(response).to have_http_status(:success)
       end
 
-      it 'get artists page' do
+      it 'gets artists page' do
         get '/artists'
         expect(response).to render_template(:index)
       end
     end
 
     context 'without authentication' do
-      it 'return fail status' do
+      it 'returns fail status' do
         get '/artists'
         expect(response).to have_http_status(302)
       end
