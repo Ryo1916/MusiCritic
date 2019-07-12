12f864c2df62e3d41046e49eb169c00c8b95e0fa
Ryo1916 <telemon1916@gmail.com>
2019-07-12 17:57:31 +0900

    Fix page_title method to display each pages title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def page_title
+  def page_title(page_title: '')
     base_title = Constants::APP_NAME
-    return base_title if @title.blank?
-    "#{base_title} | #{@title}"
+    return base_title if page_title.blank?
+    "#{page_title} | #{base_title}"
   end
 end
