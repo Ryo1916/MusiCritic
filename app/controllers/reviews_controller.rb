319a8f8cd92adf8d92e5c995a388d0cb14deea5a
Ryo1916 <telemon1916@gmail.com>
2019-07-12 17:07:40 +0900

    Change redirection to request.referer

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,7 +24,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to request.referer, notice: 'Review was successfully updated.' }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
