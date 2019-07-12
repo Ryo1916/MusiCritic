461997e3f83de25417228d5bed6dc3e0e6a64279
Ryo1916 <telemon1916@gmail.com>
2019-07-12 16:22:09 +0900

    Change after sign in path to user_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource_or_scope)
+    user_path(resource_or_scope)
+  end
+
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
