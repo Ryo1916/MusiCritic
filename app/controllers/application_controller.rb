f95505ab135be621d5f00817c7707f45572b50d7
Ryo1916 <telemon1916@gmail.com>
2019-07-17 23:43:16 +0900

    Redirect back current page after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :store_user_location!, if: :storable_location?
+
   private
 
   def after_sign_in_path_for(resource_or_scope)
-    user_path(resource_or_scope)
+    stored_location_for(resource_or_scope) || user_path(resource_or_scope)
   end
 
-  # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
 
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
 end
