c69de256acfec4d628fd35e403122f8c798310c2
Ryo1916 <telemon1916@gmail.com>
2019-07-20 19:37:03 +0900

    Set the argument limit and constant to get maximum number of artists/albums

--- app/apis/spotify_api/v2/client.rb
+++ app/apis/spotify_api/v2/client.rb
@@ -1,13 +1,14 @@
 module SpotifyAPI
   module V2
     class Client
-      def search_artist(artist_name:)
+      MAXIMUM_NUMBER_OF_RESULTS = 50
+      def search_artists(artist_name:)
         # FIXME: 空文字で検索した場合のエラーハンドリング
-        RSpotify::Artist.search(artist_name)
+        RSpotify::Artist.search(artist_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
       end
 
-      def search_album(album_name:)
-        RSpotify::Album.search(album_name)
+      def search_albums(album_name:)
+        RSpotify::Album.search(album_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
       end
 
       def search_unique_album(spotifies_album_id:)
