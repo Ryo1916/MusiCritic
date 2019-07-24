548e23b3d64bcb5bd84c4ffdda7b366b6ab27374
Ryo1916 <telemon1916@gmail.com>
2019-07-24 19:02:19 +0900

    Modify the SpotifyAPI module to include it into controllers

--- app/apis/spotify_api/v2/client.rb
+++ app/apis/spotify_api/v2/client.rb
@@ -1,17 +1,17 @@
 module SpotifyAPI
   module V2
-    class Client
+    module Client
       MAXIMUM_NUMBER_OF_RESULTS = 50
-      def search_artists(artist_name:)
+      def search_artists_from_spotify(artist_name:)
         # FIXME: 空文字で検索した場合のエラーハンドリング
         RSpotify::Artist.search(artist_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
       end
 
-      def search_albums(album_name:)
+      def search_albums_from_spotify(album_name:)
         RSpotify::Album.search(album_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
       end
 
-      def search_unique_album(spotifies_album_id:)
+      def search_unique_album_from_spotify(spotifies_album_id:)
         RSpotify::Album.find(spotifies_album_id)
       end
     end
