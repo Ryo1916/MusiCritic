f74a0ced87432d9a81cf966808c9ffe2808cce2b
Ryo1916 <telemon1916@gmail.com>
2019-07-26 19:58:29 +0900

    Set new_releases and search_unique_artist_from_spotify methods

--- app/apis/spotify_api/v2/client.rb
+++ app/apis/spotify_api/v2/client.rb
@@ -1,14 +1,23 @@
 module SpotifyAPI
   module V2
     module Client
+      RESULT_LIMITATION_OF_NEW_RELEASES = 10
       MAXIMUM_NUMBER_OF_RESULTS = 50
+
+      def new_releases
+        RSpotify::Album.new_releases(limit: RESULT_LIMITATION_OF_NEW_RELEASES)
+      end
+
       def search_artists_from_spotify(artist_name:)
-        # FIXME: 空文字で検索した場合のエラーハンドリング
         RSpotify::Artist.search(artist_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
       end
 
       def search_albums_from_spotify(album_name:)
         RSpotify::Album.search(album_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
+      end
+
+      def search_unique_artist_from_spotify(spotifies_artist_id:)
+        RSpotify::Artist.find(spotifies_artist_id)
       end
 
       def search_unique_album_from_spotify(spotifies_album_id:)
