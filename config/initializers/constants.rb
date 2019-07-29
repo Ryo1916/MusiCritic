4980b9bcf136a11a6f4511bdf9441bfbbee13267
Ryo1916 <telemon1916@gmail.com>
2019-07-29 20:14:57 +0900

    Set Some constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 module Constants
+  # base
   APP_NAME = 'Music Review'
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   DEFAULT_IMG_URL = 'https://img.pngio.com/my-my-png-album-covers-500_500.png'
+
+  # validations
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  # set ristrict
   ALBUMS_FOR_TOP_PAGE = 12
   REVIEWS_FOR_ALBUMS_SHOW_PAGE = 15
   REVIEWS_FOR_USRES_SHOW_PAGE = 15
   ARTISTS_FOR_ARTISTS_INDEX_PAGE = 24
   ALBUMS_FOR_ALBUMS_INDEX_PAGE = 24
-  MAXIMUM_NUMBER_OF_RESULTS_BY_SPOTIFY = 50
+  NEW_RELEASE_ALBUMS = 32
+
+  # Spotify API
+  MAXIMUM_RESULT_LIMITATION_OF_SPOTIFY_API = 50
 end
