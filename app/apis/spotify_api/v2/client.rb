be3d02318dc95a4bc7dadd99cf10ff321a12b320
Ryo1916 <telemon1916@gmail.com>
2019-07-29 15:04:40 +0900

    Change methods name, Set Client class instead of module

--- app/apis/spotify_api/v2/client.rb
+++ app/apis/spotify_api/v2/client.rb
@@ -1,27 +1,29 @@
 module SpotifyAPI
   module V2
-    module Client
+    class Client
       RESULT_LIMITATION_OF_NEW_RELEASES = 10
       MAXIMUM_NUMBER_OF_RESULTS = 50
 
-      def new_releases
-        RSpotify::Album.new_releases(limit: RESULT_LIMITATION_OF_NEW_RELEASES)
-      end
+      class << self
+        def new_releases
+          RSpotify::Album.new_releases(limit: RESULT_LIMITATION_OF_NEW_RELEASES)
+        end
 
-      def search_artists_from_spotify(artist_name:)
-        RSpotify::Artist.search(artist_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
-      end
+        def search_artists(artist_name:)
+          RSpotify::Artist.search(artist_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
+        end
 
-      def search_albums_from_spotify(album_name:)
-        RSpotify::Album.search(album_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
-      end
+        def search_albums(album_name:)
+          RSpotify::Album.search(album_name, limit: MAXIMUM_NUMBER_OF_RESULTS)
+        end
 
-      def search_unique_artist_from_spotify(spotifies_artist_id:)
-        RSpotify::Artist.find(spotifies_artist_id)
-      end
+        def search_unique_artist(spotifies_artist_id:)
+          RSpotify::Artist.find(spotifies_artist_id)
+        end
 
-      def search_unique_album_from_spotify(spotifies_album_id:)
-        RSpotify::Album.find(spotifies_album_id)
+        def search_unique_album(spotifies_album_id:)
+          RSpotify::Album.find(spotifies_album_id)
+        end
       end
     end
   end
