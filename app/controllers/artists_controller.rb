3e616bd4ceef12b0ef90957f61a0575c3e0bd8bf
Ryo1916 <telemon1916@gmail.com>
2019-08-01 13:51:15 +0900

    Set instance variables for artists/show page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -17,7 +17,9 @@ class ArtistsController < ApplicationController
   def show
     # FIXME: 検索せずに@artistsを利用して表示できないか？
     @artist = unique_artist(spotifies_artist_id: params[:id])
-    @albums = @artist.albums(limit: Constants::MAXIMUM_RESULT_LIMITATION_OF_SPOTIFY_API)
+    @albums = @artist.albums(limit: Constants::MAXIMUM_RESULT_LIMITATION_OF_SPOTIFY_API, offset: 0, album_type: 'album')
+    @singles = @artist.albums(limit: Constants::MAXIMUM_RESULT_LIMITATION_OF_SPOTIFY_API, offset: 0, album_type: 'single')
+    @compilations = @artist.albums(limit: Constants::MAXIMUM_RESULT_LIMITATION_OF_SPOTIFY_API, offset: 0, album_type: 'compilation')
   end
 
   def destroy
