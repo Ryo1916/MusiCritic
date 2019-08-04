6564a70c0fce19eac2a0f83357fa6f35326b4e82
Ryo1916 <telemon1916@gmail.com>
2019-08-04 18:53:46 +0900

    Add average_rating column to album

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190725104105) do
+ActiveRecord::Schema.define(version: 20190804094044) do
 
   create_table "albums", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190725104105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "reviews_count", default: 0, null: false
+    t.float "average_rating", limit: 24, default: 0.0, null: false
     t.string "spotify_id", null: false
   end
 
