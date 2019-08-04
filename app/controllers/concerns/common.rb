7a2d9c46ab827890aa01f53af558bc9e110f9cb1
Ryo1916 <telemon1916@gmail.com>
2019-08-04 16:06:26 +0900

    Delete set_instances_for_albums_show_page

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -2,7 +2,6 @@
 
 module Common
   extend ActiveSupport::Concern
-  include SpotifyAPI::V2::Client
 
   def set_user(user_id:)
     @user = User.find(user_id)
@@ -18,11 +17,5 @@ module Common
 
   def prohibit_sns_accout_users_access
     redirect_to current_user if current_user.provider.present?
-  end
-
-  def set_instances_for_albums_show_page
-    @reviews = @album.reviews.reviews_list(page: params[:page])
-    @artists = unique_album(spotifies_album_id: @album.spotify_id).artists
-    @top_reviewed_albums = Album.most_reviewed_albums(limit: Constants::ALBUMS_FOR_ALBUMS_SHOW_PAGE)
   end
 end
