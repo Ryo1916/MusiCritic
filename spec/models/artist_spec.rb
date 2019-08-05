fb0e602b01c82d9242de21952c01b7fc18c3650e
Ryo1916 <telemon1916@gmail.com>
2019-08-05 21:21:05 +0900

    Modify model spec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -15,12 +15,14 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   # Association tests
-  it { should have_many(:albums).dependent(:destroy) }
+  it { should have_many(:artists_albums).dependent(:destroy) }
+  it { should have_many(:albums).through(:artists_albums) }
 
   # Validation tests
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:image) }
   it { should validate_presence_of(:external_urls) }
+  it { should validate_presence_of(:spotify_id) }
 
   # TODO: メソッドのテスト
 end
