187b7c321b6a4461d71ea13e7a40326fa9ec716e
Ryo1916 <telemon1916@gmail.com>
2019-08-08 15:38:50 +0900

    Set artsts_album_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,12 +31,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # Associations test
+  it { should have_many(:reviews).dependent(:destroy) }
+
   # Validations test
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
-
-  # Associations test
-  it { should have_many(:reviews).dependent(:destroy) }
 
   # TODO: メソッドのテスト
 end
