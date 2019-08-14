bafcfe4149fb6a0a6b6fd698f807e2c9d5c150d5
Ryo1916 <telemon1916@gmail.com>
2019-08-15 00:52:53 +0900

    Setup for heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-
-# do not have to listen 3000
-# port        ENV.fetch("PORT") { 3000 }
-
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
