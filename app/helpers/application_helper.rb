33327aab7ace876ddfb4cdf79a272c113d4991ae
Ryo1916 <telemon1916@gmail.com>
2019-08-14 19:16:40 +0900

    Set player icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     return base_title if page_title.blank?
     "#{page_title} | #{base_title}"
   end
+
+  def play_icon(id:)
+    content_tag(:i, '', class: "fas fa-volume-up", id: "play-#{ id }", data: { play: "playable-song", id: "#{ id }"})
+  end
+
+  def stop_icon(id:)
+    content_tag(:i, '', class: "far fa-stop-circle", id: "stop-#{ id }", data: { play: "stoppable-song", id: "#{ id }"}, style: "display:none;")
+  end
 end
