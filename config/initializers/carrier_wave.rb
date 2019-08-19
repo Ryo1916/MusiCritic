4f4cb87f9a4387f2791f0b1d99a4ce94b3469511
Ryo1916 <telemon1916@gmail.com>
2019-08-19 20:22:15 +0900

    Setup carrierwave for production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,14 @@
-require 'carrierwave/orm/activerecord'
+# frozen_string_literal: true
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      region: Rails.application.secrets.region,
+      aws_access_key_id: Rails.application.secrets.access_key_id,
+      aws_secret_access_key: Rails.application.secrets.secret_access_key
+    }
+    config.fog_directory = Rails.application.secrets.bucket
+    config.asset_host = "https://s3-#{Rails.application.secrets.region}.amazonaws.com/#{Rails.application.secrets.bucket}"
+  end
+end
