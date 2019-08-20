629e8b741f8d3075496fdb91ad1faea0cb345592
Ryo1916 <telemon1916@gmail.com>
2019-08-20 17:45:10 +0900

    Change the filepath to get puma.sock

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -2,5 +2,5 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-app_root = File.expand_path("../..", __FILE__)
+app_root = File.expand_path("../../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
