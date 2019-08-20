b1e679fbf82c2b89ee3a6158a3402603b4755d31
Ryo1916 <telemon1916@gmail.com>
2019-08-20 22:11:01 +0900

    Modify puma for production

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,5 +1,18 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# frozen_string_literal: true
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "production" }
-plugin :tmp_restart
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
