5f66eac5f51dfbd5751de132c299b285c924e248
Ryo1916 <telemon1916@gmail.com>
2019-08-21 15:03:45 +0900

    Modify session store config for heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :redis_store,
-  servers: [
-   { host: ENV['REDIS_HOST'] || 'redis',
-     port: ENV['REDIS_PORT'] || 6379,
-     db: 0,
-     namespace: 'session' }
-  ],
-  expire_after: 90.minutes,
-  key: "_#{Rails.application.class.parent_name.downcase}_session"
+if Rails.env.production?
+  Rails.application.config.session_store :redis_store, {
+    servers: ENV['REDIS_URL'],
+    expire_after: 90.minutes,
+    key: "_#{Rails.application.class.parent_name.downcase}_session"
+  }
+else
+  Rails.application.config.session_store :redis_store, {
+    servers: [
+     { host: ENV['REDIS_HOST'] || 'redis',
+       port: ENV['REDIS_PORT'] || 6379,
+       db: 0,
+       namespace: 'session' }
+    ],
+    expire_after: 90.minutes,
+    key: "_#{Rails.application.class.parent_name.downcase}_session"
+  }
+end
