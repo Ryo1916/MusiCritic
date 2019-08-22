fbd9515bf00192b4e89efab06b2a238b8023dc42
Ryo1916 <telemon1916@gmail.com>
2019-08-22 20:28:18 +0900

    Change the domain for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://musicreview.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://music-review.tk' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'musicreview.herokuapp.com',
+    domain: 'music-review.tk',
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: 'apikey',
