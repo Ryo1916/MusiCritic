a9cb12374367976b2f2674abd4bdc006bf4f1244
Ryo1916 <telemon1916@gmail.com>
2019-08-23 15:51:50 +0900

    Do not redirect to new_user_session_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,6 @@ class ApplicationController < ActionController::Base
 
   def store_user_location!
     # :user is the scope we are authenticating
-    store_location_for(:user, request.fullpath)
+    store_location_for(:user, request.fullpath) unless request.fullpath != new_user_session_path
   end
 end
