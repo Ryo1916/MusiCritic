42eb097bf7e8c86acbc750b5093355a246a1353a
Ryo1916 <telemon1916@gmail.com>
2020-10-18 19:55:53 +0900

    add set_instance_variables methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def current_user?(user:)
     current_user == user
   end
+
+  def set_instance_variables(service_result)
+    @user = service_result.user
+    @reviews = service_result.reviews
+    @user_reviewed_albums = service_result.user_reviewed_albums
+    @top_rating_albums = service_result.top_rating_albums
+    @new_releases = service_result.new_releases
+  end
 end
